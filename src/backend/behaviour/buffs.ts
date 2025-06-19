9ca11d7a51a4e908955bc472eb54f01d028605f5
Алтунина Екатерина <muchashamedofyou@gmail.com>
2025-06-20 02:32:30 +0300

    implemented basic temporary bonus buff

--- src/backend/behaviour/buffs.ts
+++ src/backend/behaviour/buffs.ts
@@ -1,4 +1,5 @@
-import { Character } from './character';
+import { World, Entity } from '../../common/interfaces';
+import { Character, Characteristics } from './character';
 
 export enum TargetType {
   Self = 'SELF',
@@ -9,29 +10,113 @@ export enum TargetType {
 export enum StatType {
   Health = 'HEALTH',
   Attack = 'ATTACK',
-  Speed = 'SPEED',
+  Attribute = 'ATTRIBUTE',
 }
 
 export enum ModifierType {
   Flat = 'FLAT',
-  Add = 'ADD',
   Mult = 'MULT',
+}
+
+export type Duration = {
+  duration: number;
+  type: 'Temporary' | 'Permanent';
+};
+
+interface Bonus {
+  name: string;
+  statType: StatType;
+  attributeType?: keyof Characteristics;
+  modifierType: ModifierType;
+  value: number;
 }
 
 interface Effect {
   name: string;
-  targetType: TargetType;
-  // those three are only applicable to certain types of effects
-  statType?: StatType;
-  modifierType?: ModifierType;
-  value?: number;
-  applyEffect: () => void;
+  applyEffect: (world: World, entity: Entity) => void;
+}
+
+export function isBonus(effect: Effect | Bonus): effect is Bonus {
+  return (effect as Bonus).modifierType !== undefined;
+}
+
+export function isEffect(effect: Effect | Bonus): effect is Effect {
+  return (effect as Effect).applyEffect !== undefined;
 }
 
 export interface Buff {
   name: string;
-  duration: number;
-  maxStacks: number;
-  effect: Effect;
-  apply: (to: Character) => void;
+  targetType: TargetType;
+  duration: Duration;
+  effect: Effect | Bonus;
 }
+
+export let HealthBonus: Bonus = {
+  name: 'Health Up stats',
+  statType: StatType.Health,
+  modifierType: ModifierType.Mult,
+  value: 10,
+};
+
+export let AttributeBonus: Bonus = {
+  name: 'Attribute Up stats',
+  statType: StatType.Attribute,
+  attributeType: 's',
+  modifierType: ModifierType.Flat,
+  value: 4,
+};
+
+export let SimpleAttributeBuff: Buff = {
+  name: 'SimpleAttributeBuff',
+  targetType: TargetType.Self,
+  duration: {
+    duration: 2500,
+    type: 'Temporary',
+  },
+  effect: AttributeBonus,
+};
+
+export let SimpleHealthBuff: Buff = {
+  name: 'SimpleHealthBuff',
+  targetType: TargetType.Self,
+  duration: {
+    duration: 3000,
+    type: 'Temporary',
+  },
+  effect: HealthBonus,
+};
+
+export let Duplication: Effect = {
+  name: 'Duplication',
+  applyEffect: (world: World, entity: Entity) => {
+    console.log(`${entity} is duplicating in`, world);
+  },
+};
+
+export let Fury: Effect = {
+  name: 'Fury',
+  applyEffect: (world: World, entity: Entity) => {
+    console.log(`${entity} is enraged in`, world);
+  },
+};
+
+export let Furry: Effect = {
+  name: 'Become a Furry',
+  applyEffect: (world: World, entity: Entity) => {
+    console.log(`${entity} is now a furry`);
+  },
+};
+
+export let Pacify: Effect = {
+  name: 'Pacify',
+  applyEffect: (world: World, entity: Entity) => {
+    console.log(`${entity} is pacified in`, world);
+  },
+};
+
+export let Stun: Effect = {
+  name: 'Stun',
+  applyEffect: (world: World, entity: Entity) => {
+    console.log(`${entity} is stunned in`, world);
+  },
+};
